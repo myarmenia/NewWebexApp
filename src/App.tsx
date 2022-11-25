8ffcf81fbe184af59ff726bd71cfeb5eb30882ee
Edgar <edgaravetisyan932@gmail.com>
2022-11-25 17:33:12 +0400

    12.1

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Container from "./components/container/Container";
 import { Features } from "./components/features/Features";
 import { InfoCont } from "./components/infoCont/InfoCont";
 import Footer from "./components/footer/Footer";
-import { Header } from "./components/header/Header";
-import { News } from "./components/news/News";
+import { Header } from "./components/Header/Header";
+import { News } from "./components/News/News";
 import { Opinion } from "./components/opinion/Opinion";
-import { Questions } from "./components/questions/Questions";
+import { Questions } from "./components/Questions/Questions";
 import { SearchArea } from "./components/searchArea/SearchArea";
 import { Lessons } from "./components/lessons/Lessons";
 
--- src/components/header/SmallMenu/SmallMenu.tsx
+++ src/components/header/SmallMenu/SmallMenu.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Navbar } from "../navbar/Navbar";
-import "./SmallMenu.css";
+import "./smallMenu.css";
 
 interface SmallMEnuProps {
   animActive: boolean;
