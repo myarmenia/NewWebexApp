8cf7ee549795712ed27169ec82d456eb21a82a84
Edgar <112865465+Edgar1824@users.noreply.github.com>
2022-12-24 14:58:12 +0400

    Delete src directory

--- src/pages/teacher_page/lessons/firstLesson/firstLesCont/BoxTitle.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface BoxTitleProps {
-  title: string;
-}
-
-export const BoxTitle: React.FC<BoxTitleProps> = ({ title }) => {
-  return <div className="text-[#6B6B6B] text-base mb-3">{title}</div>;
-};
