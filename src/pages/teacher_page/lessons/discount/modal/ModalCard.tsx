fad0429ea8906bfe07cccb365d08e1092ca5d6ae
rubsargsyann <ruben.sargsyan20012018@gmail.com>
2023-01-23 17:37:34 +0400

    allStyleChanges

--- src/pages/teacher_page/lessons/discount/modal/ModalCard.tsx
+++ src/pages/teacher_page/lessons/discount/modal/ModalCard.tsx
@@ -1,17 +1,18 @@
 import React, { FC } from "react";
 import cardImg from "../../../../../assets/teacher_images/discount/discountCard.svg";
 import deleteImg from "../../../../../assets/teacher_images/discount/delete.svg";
+import styles from "./discountModal.module.css";
 const ModalCard: FC = () => {
   return (
-    <div className="modalCardChild">
-      <div className="firstCardSection">
+    <div className={styles.modalCardChild}>
+      <div className={styles.firstCardSection}>
         <img src={cardImg} />
-        <div className="firstCardText">
+        <div className={styles.firstCardText}>
           <div>Դասընթացի վերնագիրը կարող է լինել 2 տողով</div>
           <div>Գրաֆիկ դիզայնի դասընթաց սկսնակների համար</div>
         </div>
       </div>
-      <div className="secondCardSection">
+      <div className={styles.secondCardSection}>
         <div>65 000դրամ</div>
         {/* <button type="button"> */}
         <img src={deleteImg} />
--- src/pages/teacher_page/lessons/discount/modal/ModalDiscount.tsx
+++ src/pages/teacher_page/lessons/discount/modal/ModalDiscount.tsx
@@ -1,7 +1,7 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { FC } from "react";
 import { FormProvider, useForm } from "react-hook-form";
-import "./discountModal.css";
+import styles from "./discountModal.module.css";
 import ModalCard from "./ModalCard";
 import { CstmInput, CustomSelect } from "../../../../../components/forms";
 import { ModalContainer } from "../../../../../components/modalContainer/ModalContainer";
@@ -37,24 +37,25 @@ export const ModalDiscount: FC<IModalDiscount> = ({ modalActive }) => {
         <form className="modalChild" onSubmit={handleSubmit(onSubmit)}> */}
       <ModalContainer onClick={modalActive}>
         <form onSubmit={handleSubmit(onSubmit)} action="">
-          <div className="modalTitle">Ակտիվացնել Զեղչ</div>
-          <div className="modalInput">
-            <div className="modalInputChild">
-              <div className="modalInpTitle">Զեղչի քանակ</div>
-              <div className="modalInp">
+          <div className={styles.modalTitle}>Ակտիվացնել Զեղչ</div>
+          <div className={styles.modalInput}>
+            <div className={styles.modalInputChild}>
+              <div className={styles.modalInpTitle}>Զեղչի քանակ</div>
+              <div className={styles.modalInp}>
                 <CstmInput
-                  className="modal-inp1"
+                  className={styles["modal-inp1"]}
                   placeholder="Արժեք"
                   regName="price"
                 />
                 <CustomSelect
                   placeholder="Տեսակ"
                   regName="select"
-                  className="registration_select"
+                  // className="registration_select"
+
                   options={["Տոկոս %", "Դրամ"]}
                 />
               </div>
-              <div className="modalInpTitle">Դասընթացներ</div>
+              <div className={styles.modalInpTitle}>Դասընթացներ</div>
               <CustomSelect
                 placeholder="Դասընթացի վերնագիրը"
                 regName="titleSelect"
@@ -67,40 +68,42 @@ export const ModalDiscount: FC<IModalDiscount> = ({ modalActive }) => {
                 ]}
               />
             </div>
-            <div className="modalInputChild2">
-              <div className="modalInpTitle">Ժամանակահատված</div>
-              <div className="date">
+            <div className={styles.modalInputChild2}>
+              <div className={styles.modalInpTitle}>Ժամանակահատված</div>
+              <div className={styles.date}>
                 <input
                   type="date"
-                  className="dateInp"
+                  className={styles.dateInp}
                   {...register("date.start", { value: new Date() })}
                 />
-                <div className="gic"></div>
+                <div className={styles.gic}></div>
                 <input
                   type="date"
-                  className="dateInp"
+                  className={styles.dateInp}
                   {...register("date.end", { value: new Date() })}
                 />
               </div>
-              <div className="dateError">{errors.date?.end?.message}</div>
-              <div className="modalCheckbox">
+              <div className={styles.dateError}>
+                {errors.date?.end?.message}
+              </div>
+              <div className={styles.modalCheckbox}>
                 <input
                   type="checkbox"
-                  className="modalCheck"
+                  className={styles.modalCheck}
                   {...register("timeCheck")}
                 />
-                <div className="checkboxText">Անժամկետ</div>
+                <div className={styles.checkboxText}>Անժամկետ</div>
               </div>
             </div>
           </div>
-          <div className="modalCard">
+          <div className={styles.modalCard}>
             <ModalCard />
             <ModalCard />
             <ModalCard />
             <ModalCard />
           </div>
 
-          <div className="modalButton">
+          <div className={styles.modalButton}>
             <button onClick={modalActive} type="button">
               Չեղարկել
             </button>
